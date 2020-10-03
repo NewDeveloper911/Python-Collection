e25636c068cbf1ff96c511ea4d3c82b1c426021e
NewDeveloper911 <62492531+NewDeveloper911@users.noreply.github.com>
2020-10-03 22:48:06 +0400

    Update pyserver.py
    I have merely stated where you can change the directories to find your music

--- flask_todo/pyserver.py
+++ flask_todo/pyserver.py
@@ -11,7 +11,7 @@ def speak(text):
     play.playsound(filename)
 
 def funkymusic(songfile):
-    for root, dirs, files in os.walk(r'D:\Nanayaw\Demo Songs'):
+    for root, dirs, files in os.walk(r'D:\Nanayaw\Demo Songs'): # This directory is just where my songs are. change code to suit your website
         for name in files:
             if name == songfile:
                 print("Now playing - " + songfile)
