6473068fec5622163f361ce32390520901d0b24f
Brobbey Brobbey <62492531+NewDeveloper911@users.noreply.github.com>
2020-10-04 20:47:37 +0400

    flask server update

--- flask_todo/hello.py
+++ flask_todo/hello.py
@@ -1,19 +1,23 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 import pyserver as song
 
 @app.route("/<name>")
 def homepage(name):
     return render_template("flaskindex.html", content=name)
-'''    
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-    
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin"))
-'''
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"] #gets the data from the 'nm' key in a dictionary
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    print(usr)
+    return f"<p>{usr}</p>"
 
 @app.route('/play')
 def playsong():
--- flask_todo/pyserver.py
+++ flask_todo/pyserver.py
@@ -11,7 +11,7 @@ def speak(text):
     play.playsound(filename)
 
 def funkymusic(songfile):
-    for root, dirs, files in os.walk(r'D:\Nanayaw\Demo Songs'): # This directory is just where my songs are. change code to suit your website
+    for root, dirs, files in os.walk(r'D:\Nanayaw\Demo Songs'):
         for name in files:
             if name == songfile:
                 print("Now playing - " + songfile)
