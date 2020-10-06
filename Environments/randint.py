6f2b01047e04f8772314fc0aed90b63446b815fa
Brobbey Brobbey <62492531+NewDeveloper911@users.noreply.github.com>
2020-10-06 21:15:41 +0400

    Extra organisation
    Deleting some of my old files which have already been moved

--- Environments/randint.py
+++
@@ -1,5 +0,0 @@
-import math
-from random import randint
-
-for i in range(25):
-	print(randint(1, 25))
--- Environments/server.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-secmessage = "omae wo mo shinderu"
-app = Flask(__name__)
-
-@app.route("/")
-def get_secret_message():
-	return secmessage
