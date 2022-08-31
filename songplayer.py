d9004fdba21bec392824fcc5029f8f2a93a5e0c5
NewDeveloper911 <nanabrobbey@aaess.sch.ae>
2022-08-31 11:38:57 +0400

    Laptop workaround

--- songplayer.py
+++ songplayer.py
@@ -3,11 +3,13 @@ import os
 import time
 import playsound as play
 
+
 def speak(text):
     tts = gTTS(text=text, lang='en')
     filename = 'voice.mp3'
     tts.save(filename)
     play.playsound(filename)
+
 
 def funkymusic(songfile):
     for root, dirs, files in os.walk(r'/'):
@@ -17,9 +19,10 @@ def funkymusic(songfile):
                 speak("Now playing - {}".format(songfile[:-4]))
                 play.playsound(os.path.abspath(os.path.join(root, name)))
 
-song = input("Would you like to play a song? If so, which song would you like to play?\n Don't forget to state the directory, if possible\n")
+
+song = input("Would you like to play a song? If so, what would you like to play?\n Don't forget to state the directory, if possible\n")
 try:
-	funkymusic(song)
+    funkymusic(song)
 except:
-	print("Unfortunately, we couldn't find and play this song for you")
+    print("Unfortunately, we couldn't find and play this song for you")
 print("Thanks for utilising this python script")
